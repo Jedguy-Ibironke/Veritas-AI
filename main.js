4672b555059f215b88a9d3d27c18f1d01a1c973f
Jedguy-Ibironke <72940965+Jedguy-Ibironke@users.noreply.github.com>
2026-02-20 14:53:00 -0700

    Added changes

--- main.js
+++ main.js
@@ -1,30 +1,42 @@
-/*
 import { startWebcam } from "./camera.js";
 import { detectLandmarks } from "./landmarks.js";
+import {
+	computeMouthOpen,
+	computeInstability,
+	computeRisk,
+	getRiskLabel,
+} from "./metrics.js";
 
 const video = document.getElementById("video");
+const status = document.getElementById("status");
 
 startWebcam(video)
 	.then(() => {
 		console.log("Camera started");
-
-		setInterval(async () => {
-			const landmarks = await detectLandmarks(video);
-			console.log(landmarks);
-		}, 500);
+		startDetection();
 	})
 	.catch((err) => console.error("Camera error:", err));
-*/
-import { startWebcam } from "./camera.js";
-import { detectLandmarks } from "./landmarks.js";
 
-const video = document.getElementById("video");
-
-startWebcam(video).then(() => {
-	console.log("Camera started");
-
-	setInterval(async () => {
+async function startDetection() {
+	async function detect() {
 		const landmarks = await detectLandmarks(video);
-		console.log("Landmarks:", landmarks);
-	}, 1000);
-});
+
+		if (landmarks) {
+			const mouthOpen = computeMouthOpen(landmarks);
+			const instability = computeInstability(landmarks);
+
+			const risk = computeRisk(instability, mouthOpen);
+			const label = getRiskLabel(risk);
+
+			status.innerText = `
+Mouth: ${mouthOpen.toFixed(3)}
+Instability: ${instability.toFixed(3)}
+Risk: ${risk.toFixed(3)} (${label})
+      `;
+		}
+
+		requestAnimationFrame(detect);
+	}
+
+	detect();
+}
